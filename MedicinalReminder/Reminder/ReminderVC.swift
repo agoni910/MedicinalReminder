faa92754fb03dd125723580e38a5da24606dcf4a
agoni910 <2864781363@qq.com>
2023-03-12 22:26:27 +0800

    add 提醒tab分页外观

--- MedicinalReminder/Reminder/ReminderVC.swift
+++ MedicinalReminder/Reminder/ReminderVC.swift
@@ -11,6 +11,16 @@ import XLPagerTabStrip
 class ReminderVC: ButtonBarPagerTabStripViewController {
 
     override func viewDidLoad() {
+        
+        settings.style.selectedBarBackgroundColor = .systemIndigo
+        settings.style.selectedBarHeight = 3
+        
+        settings.style.buttonBarItemBackgroundColor = .clear
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemFont = .systemFont(ofSize: 16)
+        
+        settings.style.buttonBarItemLeftRightMargin = 0
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
